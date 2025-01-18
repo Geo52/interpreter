e6466c07f7e40f562aab2178088ccf3f795fba13
Jorge Rios <jorgeluisrios47@gmail.com>
2025-01-18 17:13:59 -0600

    lexing single character tokens is almost done

--- lexer.c
+++ lexer.c
@@ -1,32 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 typedef enum
 {
-    // Single-character tokens
+    // Single-character tokens.
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
     PLUS,
-
-    // One or two character tokens
-
-    // Literals
-
-    // Keywords
-
-
-
+    SEMICOLON,
+    SLASH,
+    STAR,
 } Token_Type;
 
-// define a token
 typedef struct
 {
-    Token_Type type; 
-    char value;
+    Token_Type type;
+    char *literal;
+
 } Token;
+
+Token tokenCreate(Token_Type type, char *literal)
+{
+    Token token;
+    token.type = type;
+    token.literal = literal;
+    return token;
+}
+
+Token tokenizer(char characterInQuestion, Token token)
+{
+    switch (characterInQuestion)
+    {
+    case '(':
+        // when you find a token intialize it
+        token = tokenCreate(LEFT_PAREN, NULL);
+        break;
+    case ')':
+        token = tokenCreate(RIGHT_PAREN, NULL);
+        break;
+    case '{':
+        token = tokenCreate(LEFT_BRACE, NULL);
+        break;
+    case '}':
+        token = tokenCreate(RIGHT_BRACE, NULL);
+        break;
+    case ',':
+        token = tokenCreate(COMMA, NULL);
+        break;
+    case '.':
+        token = tokenCreate(DOT, NULL);
+        break;
+    case '-':
+        token = tokenCreate(MINUS, NULL);
+        break;
+    case '+':
+        token = tokenCreate(PLUS, NULL);
+        break;
+    case ';':
+        token = tokenCreate(SEMICOLON, NULL);
+        break;
+    case '/':
+        token = tokenCreate(SLASH, NULL);
+        break;
+    case '*':
+        token = tokenCreate(STAR, NULL);
+        break;
+    default:
+        break;
+    }
+    return token;
+}
 
 int main()
 {
     // hard coded example source code
-    char source_code[] = "let x = 5 + 5;";
+    char source_code[] = "(){},.-+;/*";
 
+    // loop through the source code on character at a time
+    for (int i = 0; source_code[i] != '\0'; i++)
+    {
+        // ignore whitespace
+        if (isspace(source_code[i]))
+        {
+            continue;
+        }
 
+        Token token = tokenizer(source_code[i], token);
+
+        printf("Token Type: %d\n", token.type);
+        printf("Token literal: %s\n", token.literal);
+    }
 }
